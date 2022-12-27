0d39dd05cbd08ce5b5c965e6915728a5bf4e9558
Yanli Wang <yw7bh@CENGR-FZ2F6S3.engineering.missouri.edu>
2022-12-27 00:09:05 -0600

    training

--- ScHiCedsr_train.py
+++ ScHiCedsr_train.py
@@ -1,9 +1,27 @@
 import sys
 sys.path.append('.')
-sys.path.append('../')
 
 from Pretrain.train_hiedsr_vH import hiedsr
 import argparse
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description = 'Training process for ScHiCedsr or ScHiCedsrgan.')
+    parser.add_argument('--gan', type = int, choices=[0, 1], default=0)
+    parser.add_argument('--epoch', type = int, default=300)
+    parser.add_argument('--batch_size', type = int, default=64)
+    parser.add_argument('--celln', type = int, default=1, help='The cell number of the cell-line you want to train')
+    parser.add_argument('--celline', type = str, default='Human')
+    parser.add_argument('--percent', type = float, default='0.75', help = 'The down-sampling ratio for the raw input dataset, the value should be equal or larger than 0.02 but nt larger than 1.0')
+    args = parser.parse_args()
+
+
+    if args.gan == 0:
+        train_model = hiedsr(Gan=False, epoch=args.epoch, batch_s=args.batch_size, cellN=args.celln, celline=args.celline, percentage=args.percent)
+        train_model.fit_model()
+        print("\n\nTraining ScHiCedsr is done!!!\n")
+    else:
+        train_model = hiedsr(Gan=True, epoch=args.epoch, batch_s=args.batch_size, cellN=args.celln, celline=args.celline, percentage=args.percent)
+        train_model.fit_model()
+        print("\n\nTraining ScHiCedsrgan is done!!!\n")
+
+
